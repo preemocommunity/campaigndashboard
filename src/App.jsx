ae111031515f1f288b980def3bdf364d949c70d7
Prëmo <95042253+preemocommunity@users.noreply.github.com>
2026-04-19 13:19:45 +0800

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CampaignDashboard from "./CampaignDashboard";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <CampaignDashboard />
+  </React.StrictMode>
+);
