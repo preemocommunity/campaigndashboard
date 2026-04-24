bba72bb000b7e7663e7a1ec6f77ff37c9c0d4ad1
Prëmo <95042253+preemocommunity@users.noreply.github.com>
2026-04-24 13:35:09 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import CampaignDashboard from "./CampaignDashboard";
+import CampaignDashboard from "./CampaignDashboard-v2";
 
 export default function App() {
   return <CampaignDashboard />;
